a11de02754c0124997dbb6fbb77687db07a447ad
Daniel Schauenberg <d@unwiredcouch.com>
2011-06-24 22:50:14 +0200

    add basic API lib and a lot of failing tests

---
+++ simplenote/simplenote.py
@@ -0,0 +1,198 @@
+# -*- coding: utf-8 -*-
+"""
+    simplenote.py
+    ~~~~~~~~~~~~~~
+
+    Python library for accessing the Simplenote API
+
+    :copyright: (c) 2011 by Daniel Schauenberg
+    :license: MIT, see LICENSE for more details.
+"""
+
+import urllib2
+import base64
+import json
+
+AUTH_URL = 'https://simple-note.appspot.com/api/login'
+DATA_URL = 'https://simple-note.appspot.com/api2/data'
+INDX_URL = 'https://simple-note.appspot.com/api2/index?'
+NOTE_FETCH_LENGTH = 20
+
+class Simplenote(object):
+
+    def __init__(self, username, password):
+        """ object constructor """
+        self.username = urllib2.quote(username)
+        self.password = urllib2.quote(password)
+        self.token = None
+
+    def authenticate(self, user, password):
+        """ function to get simplenote auth token
+
+        Arguments:
+        user     -- simplenote email address
+        password -- simplenote password
+
+        Returns:
+        Simplenote API token
+
+        """
+        auth_params = "email=%s&password=%s" % (user, password)
+        values = base64.encodestring(auth_params)
+        request = urllib2.Request(AUTH_URL, values)
+        try:
+            res = urllib2.urlopen(request).read()
+            token = urllib2.quote(res)
+        except IOError: # no connection exception
+            token = None
+        return token
+
+    def get_token(self):
+        """ function to retrieve an auth token. It returns the cached global
+        token or retrieves a new one if there isn't one
+
+        Returns
+        the simplenote API token
+
+        """
+        if self.token == None:
+            self.token = self.authenticate(self.username, self.password)
+        return self.token
+
+
+    def get_note(self, noteid):
+        """ function to get a specific note
+
+        Arguments
+        noteid -- ID of the note to get
+
+        Returns
+        the desired note
+
+        """
+        # request note
+        params = '/%s?auth=%s&email=%s' % (str(noteid), self.get_token(),
+                                           self.username)
+        request = urllib2.Request(DATA_URL+params)
+        try:
+            response = urllib2.urlopen(request)
+        except IOError, e:
+            return e, False
+        note = json.loads(response.read())
+        # use UTF-8 encoding
+        note["content"] = note["content"].encode('utf-8')
+        note["tags"] = [t.encode('utf-8') for t in note["tags"]]
+        return note, True
+
+    def update_note(self, note):
+        """ function to update a specific note object, if the note object does not
+        have a "key" field, a new note is created
+
+        Arguments
+        note  -- note object to update
+
+        Returns
+        True and the JSON parsed response on success,
+        False with error message otherwise
+
+        """
+        # use UTF-8 encoding
+        note["content"] = unicode(note["content"], 'utf-8')
+        if note.has_key("tags"):
+            note["tags"] = [unicode(t, 'utf-8') for t in note["tags"]]
+
+        # determine whether to create a new note or updated an existing one
+        if note.has_key("key"):
+            url = '%s/%s?auth=%s&email=%s' % (DATA_URL, note["key"],
+                                              self.get_token(), self.username)
+        else:
+            url = '%s?auth=%s&email=%s' % (DATA_URL, self.get_token(), self.username)
+        request = urllib2.Request(url, json.dumps(note))
+        response = ""
+        try:
+            response = urllib2.urlopen(request).read()
+        except IOError, e:
+            return e, False
+        return json.loads(response), True
+
+    def add_note(self, note):
+        """wrapper function to add a note
+
+        Arguments
+        note -- the note to add
+
+        Returns
+        The newly created note and a success status
+        """
+        pass
+
+    def get_note_list(self):
+        """ function to get the note list
+
+        Returns:
+        list of note titles and success status
+
+        """
+        # initialize data
+        status = 0
+        ret = []
+        response = {}
+        notes = { "data" : [] }
+
+        # get the full note index
+        params = 'auth=%s&email=%s&length=%s' % (self.get_token(), self.username,
+                                                 NOTE_FETCH_LENGTH)
+        # perform initial HTTP request
+        try:
+            request = urllib2.Request(INDX_URL+params)
+            response = json.loads(urllib2.urlopen(request).read())
+            notes["data"].extend(response["data"])
+        except IOError:
+            status = -1
+
+        # get additional notes if bookmark was set in response
+        while response.has_key("mark"):
+            vals = (self.get_token(), self.username, response["mark"], NOTE_FETCH_LENGTH)
+            params = 'auth=%s&email=%s&mark=%s&length=%s' % vals
+
+            # perform the actual HTTP request
+            try:
+                request = urllib2.Request(INDX_URL+params)
+                response = json.loads(urllib2.urlopen(request).read())
+                notes["data"].extend(response["data"])
+            except IOError:
+                status = -1
+
+        # parse data fields in response
+        ret = notes["data"]
+
+        return ret, status
+
+    def trash_note(self, note_id):
+        """ function to move a note to the trash
+
+        Arguments
+        note_id -- id of the note to trash
+
+        Returns
+        list of note titles and success status
+
+        """
+        # get note
+        note = self.get_note(note_id)
+        # set deleted property
+        note["deleted"] = 1
+        # update note
+        return self.update_note(note)
+
+    def delete_note(self, note_id):
+        """ function to permanently delete a note
+
+        Arguments:
+        note_id -- id of the note to delete
+
+        Returns:
+        true and "OK." on success, false and err msg on error
+
+        """
+        pass
--- tests/test_simplenote.py
+++ tests/test_simplenote.py
@@ -1,4 +1,4 @@
-# encoding: utf-8
+# -*- coding: utf-8 -*-
 import unittest
 import os
 import sys
@@ -8,36 +8,101 @@ from simplenote import Simplenote
 class TestNotifyUser(unittest.TestCase):
 
     def setUp(self):
-        self.user = "test_provider"
-        self.password = "test_user"
+        self.user = "simplenote-test@lordofhosts.de"
+        self.password = "foobar"
+        self.unicode_note = "∮ E⋅da = Q,  n → ∞, ∑ f(i) = ∏ g(i),      ⎧⎡⎛┌─────┐⎞⎤⎫"
+        Simplenote(self.user, self.password).add_note("First Note.")
+        Simplenote(self.user, self.password).add_note("Second Note.")
+
+    def tearDown(self):
+        res, status = Simplenote(self.user, self.password).get_note_list()
+        [Simplenote(self.user, self.password).delete_note(n["key"]) for n in res]
+
+    def test_simplenote_auth(self):
+        token = Simplenote(self.user, self.password).get_token()
+        self.assertNotEqual(None, token)
+
+    def test_simplenote_failed_auth(self):
+        token = Simplenote(self.user, "").get_token()
+        self.assertEqual(None, token)
 
     def test_simplenote_get_list_length(self):
-        res = Simplenote(self.user, self.password).get_note_list()
+        res, status = Simplenote(self.user, self.password).get_note_list()
         self.assertEqual(2, len(res))
 
+    def test_simplenote_get_list_status(self):
+        res, status = Simplenote(self.user, self.password).get_note_list()
+        self.assertEqual(0, status)
+
     def test_simplenote_first_note(self):
-        pass
+        res, status = Simplenote(self.user, self.password).get_note_list()
+        note, status = Simplenote(self.user, self.password).get_note(res[0]["key"])
+        self.assertTrue(type(note) == dict)
+        self.assertEqual(0, status)
+        self.assertEqual("First Note.", note["content"].split('\n')[0])
 
     def test_simplenote_second_note(self):
-        pass
+        res, status = Simplenote(self.user, self.password).get_note_list()
+        note, status = Simplenote(self.user, self.password).get_note(res[1]["key"])
+        self.assertTrue(type(note) == dict)
+        self.assertEqual(0, status)
+        self.assertEqual("Second Note.", note["content"].split('\n')[0])
 
     def test_simplenote_trash_note(self):
-        pass
+        res, status = Simplenote(self.user, self.password).get_note_list()
+        note, status = Simplenote(self.user, self.password).trash_note(res[0]["key"])
+        note, status = Simplenote(self.user, self.password).get_note(res[0]["key"])
+        self.assertEqual(0, status)
+        self.assertEqual(1, note["deleted"])
 
     def test_simplenote_delete_note(self):
-        pass
+        res, status = Simplenote(self.user, self.password).get_note_list()
+        note, status = Simplenote(self.user, self.password).get_note(res[0]["key"])
+        # note exists
+        self.assertEqual(0, status)
+        note, status = Simplenote(self.user, self.password).delete_note(res[0]["key"])
+        # deletion successful
+        self.assertEqual(0, status)
+        note, status = Simplenote(self.user, self.password).get_note(res[0]["key"])
+        # note does not exist anymore
+        self.assertEqual(-1, status)
 
-    def test_simplenote_add_note(self):
-        pass
+    def test_simplenote_add_note_object(self):
+        res, status = Simplenote(self.user, self.password).add_note({"content":
+                                                                     "new note"})
+        note, status = Simplenote(self.user, self.password).get_note(res["key"])
+        self.assertEqual(0, status)
+        self.assertEqual("new note", note["content"])
+
+    def test_simplenote_add_note_content(self):
+        res, status = Simplenote(self.user, self.password).add_note("new note")
+        note, status = Simplenote(self.user, self.password).get_note(res["key"])
+        self.assertEqual(0, status)
+        self.assertEqual("new note", note["content"])
 
     def test_simplenote_update_note(self):
-        pass
+        res, status = Simplenote(self.user, self.password).get_note_list()
+        note, status = Simplenote(self.user, self.password).get_note(res[0]["key"])
+        note["content"] = "Updated note."
+        note, status = Simplenote(self.user, self.password).update_note(note)
+        note, status = Simplenote(self.user, self.password).get_note(note["key"])
+        self.assertEqual(0, status)
+        self.assertEqual("Updated Note.", note["content"].split('\n')[0])
 
-    def test_simplenote_is_utf8(self):
-        pass
+    def test_simplenote_is_unicode(self):
+        res, status = Simplenote(self.user, self.password).get_note_list()
+        note, status = Simplenote(self.user, self.password).get_note(res[0]["key"])
+        self.assertTrue(type(note["content"]) == unicode)
 
-    def test_simplenote_update_utf8(self):
-        pass
+    def test_simplenote_update_unicode(self):
+        res, status = Simplenote(self.user, self.password).get_note_list()
+        note, status = Simplenote(self.user, self.password).get_note(res[0]["key"])
+        note["content"] = self.unicode_note
+        note, status = Simplenote(self.user, self.password).update_note(note)
+        note, status = Simplenote(self.user, self.password).get_note(note["key"])
+        self.assertTrue(type(note["content"]) == unicode)
 
 if __name__ == '__main__':
     unittest.main()
+
+
