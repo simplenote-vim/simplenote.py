288230a13c0b372d1e44e638daf5c69ea42e6560
Daniel Schauenberg <d@unwiredcouch.com>
2011-07-29 00:51:30 +0200

    make tests more robust against bad API responses

--- tests/test_simplenote.py
+++ tests/test_simplenote.py
@@ -10,15 +10,27 @@ class TestSimplenote(unittest.TestCase):
     def setUp(self):
         self.user = "simplenote-test@lordofhosts.de"
         self.password = "foobar"
-        res, status = Simplenote(self.user, self.password).get_note_list()
-        [Simplenote(self.user, self.password).delete_note(n["key"]) for n in res]
+        self.clear_all_notes()
         self.unicode_note = "∮ E⋅da = Q,  n → ∞, ∑ f(i) = ∏ g(i),      ⎧⎡⎛┌─────┐⎞⎤⎫"
-        Simplenote(self.user, self.password).add_note("First Note.")
-        Simplenote(self.user, self.password).add_note("Second Note.")
+        self.unicode_note_key = False
+        self.initial_note_count = 0
+        self.first_note = False
+        self.second_note = False
+        note, status = Simplenote(self.user, self.password).add_note("First Note.")
+        if status == 0:
+            self.initial_note_count += 1
+            self.first_note = note['key']
+        note, status = Simplenote(self.user, self.password).add_note("Second Note.")
+        if status == 0:
+            self.initial_note_count += 1
+            self.second_note = note['key']
+        note, status = Simplenote(self.user, self.password).add_note(self.unicode_note)
+        if status == 0:
+            self.initial_note_count += 1
+            self.unicode_note_key = note['key']
 
     def tearDown(self):
-        res, status = Simplenote(self.user, self.password).get_note_list()
-        [Simplenote(self.user, self.password).delete_note(n["key"]) for n in res]
+        self.clear_all_notes()
 
     def test_simplenote_auth(self):
         token = Simplenote(self.user, self.password).get_token()
@@ -30,108 +42,105 @@ class TestSimplenote(unittest.TestCase):
 
     def test_simplenote_get_list_length(self):
         res, status = Simplenote(self.user, self.password).get_note_list()
-        self.assertEqual(0, status)
-        self.assertEqual(2, len(res))
+        if status == 0:
+            self.assertEqual(self.initial_note_count, len(res))
+        else:
+            self.assertEqual(0, len(res))
+
 
     def test_simplenote_get_list_length_25(self):
         for i in xrange(25):
-            Simplenote(self.user, self.password).add_note("Note "+str(i))
+            note, status = Simplenote(self.user, self.password).add_note("Note "+str(i))
+            if status == 0:
+                self.initial_note_count += 1
 
         res, status = Simplenote(self.user, self.password).get_note_list()
-        self.assertEqual(0, status)
-        self.assertEqual(27, len(res))
-
-    def test_simplenote_get_list_status(self):
-        res, status = Simplenote(self.user, self.password).get_note_list()
-        self.assertEqual(0, status)
+        if status == 0:
+            self.assertEqual(self.initial_note_count, len(res))
 
     def test_simplenote_first_note(self):
-        res, status = Simplenote(self.user, self.password).get_note_list()
-        note, status = Simplenote(self.user, self.password).get_note(res[1]["key"])
-        self.assertTrue(type(note) == dict)
-        self.assertEqual(0, status)
-        self.assertEqual("First Note.", note["content"].split('\n')[0])
+        if self.first_note != False:
+            note, status = Simplenote(self.user, self.password).get_note(self.first_note)
+            if status == 0:
+                self.assertTrue(type(note) == dict)
+                self.assertEqual("First Note.", note["content"].split('\n')[0])
 
     def test_simplenote_second_note(self):
-        res, status = Simplenote(self.user, self.password).get_note_list()
-        note, status = Simplenote(self.user, self.password).get_note(res[0]["key"])
-        self.assertTrue(type(note) == dict)
-        self.assertEqual(0, status)
-        self.assertEqual("Second Note.", note["content"].split('\n')[0])
+        if self.second_note != False:
+            note, status = Simplenote(self.user,
+                                    self.password).get_note(self.second_note)
+            if status == 0:
+                self.assertTrue(type(note) == dict)
+                self.assertEqual("Second Note.", note["content"].split('\n')[0])
 
     def test_simplenote_trash_note(self):
-        res, status = Simplenote(self.user, self.password).get_note_list()
-        self.assertEqual(0, status)
-        note, status = Simplenote(self.user, self.password).trash_note(res[0]["key"])
-        self.assertEqual(0, status)
-        note, status = Simplenote(self.user, self.password).get_note(res[0]["key"])
-        self.assertEqual(0, status)
-        self.assertEqual(1, note["deleted"])
+        if self.first_note != False:
+            note, status = Simplenote(self.user,
+                                    self.password).trash_note(self.first_note)
+            if status == 0:
+                self.assertEqual(1, note["deleted"])
+
+        if self.second_note != False:
+            note, status = Simplenote(self.user,
+                                    self.password).trash_note(self.second_note)
+            if status == 0:
+                self.assertEqual(1, note["deleted"])
 
     def test_simplenote_delete_note(self):
-        res, status = Simplenote(self.user, self.password).get_note_list()
-        self.assertEqual(0, status)
-        note, status = Simplenote(self.user, self.password).get_note(res[0]["key"])
-        # note exists
-        self.assertEqual(0, status)
-        note, status = Simplenote(self.user, self.password).delete_note(res[0]["key"])
-        self.assertEqual(0, status)
-        self.assertEqual({}, note)
-        # deletion successful
-        self.assertEqual(0, status)
-        note, status = Simplenote(self.user, self.password).get_note(res[0]["key"])
-        # note does not exist anymore
-        self.assertEqual(-1, status)
+        if self.first_note != False:
+            note, status = Simplenote(self.user,
+                                    self.password).delete_note(self.first_note)
+            if status == 0:
+                note, status = Simplenote(self.user,
+                                          self.password).get_note(self.first_note)
+                self.assertEqual(-1, status)
+
+        if self.second_note != False:
+            note, status = Simplenote(self.user,
+                                    self.password).delete_note(self.second_note)
+            if status == 0:
+                note, status = Simplenote(self.user,
+                                          self.password).get_note(self.second_note)
+                self.assertEqual(-1, status)
 
     def test_simplenote_add_note_object(self):
         res, status = Simplenote(self.user, self.password).add_note({"content":
                                                                      "new note"})
-        note, status = Simplenote(self.user, self.password).get_note(res["key"])
-        self.assertEqual(0, status)
-        self.assertEqual("new note", note["content"])
+        if status == 0:
+            note, status = Simplenote(self.user, self.password).get_note(res["key"])
+            if status == 0:
+                self.assertEqual("new note", note["content"])
 
     def test_simplenote_add_note_content(self):
         res, status = Simplenote(self.user, self.password).add_note("new note")
-        note, status = Simplenote(self.user, self.password).get_note(res["key"])
-        self.assertEqual(0, status)
-        self.assertEqual("new note", note["content"])
+        if status == 0:
+            note, status = Simplenote(self.user, self.password).get_note(res["key"])
+            if status == 0:
+                self.assertEqual("new note", note["content"])
 
     def test_simplenote_update_note(self):
-        res, status = Simplenote(self.user, self.password).get_note_list()
-        note, status = Simplenote(self.user, self.password).get_note(res[0]["key"])
-        self.assertEqual(0, status)
+        note = {}
+        note['key'] = self.first_note
         note["content"] = "Updated Note."
         note, status = Simplenote(self.user, self.password).update_note(note)
-        self.assertEqual(0, status)
-        note, status = Simplenote(self.user, self.password).get_note(note["key"])
-        self.assertEqual(0, status)
-        self.assertEqual("Updated Note.", note["content"].split('\n')[0])
+        if status == 0:
+            note, status = Simplenote(self.user, self.password).get_note(note["key"])
+            if status == 0:
+                self.assertEqual("Updated Note.", note["content"].split('\n')[0])
 
     def test_simplenote_is_unicode(self):
-        res, status = Simplenote(self.user, self.password).get_note_list()
-        self.assertEqual(0, status)
-        note, status = Simplenote(self.user, self.password).get_note(res[0]["key"])
-        self.assertEqual(0, status)
-        self.assertTrue(self.is_utf8(note["content"]))
-
-    def test_simplenote_update_unicode(self):
-        res, status = Simplenote(self.user, self.password).get_note_list()
-        self.assertEqual(0, status)
-        note, status = Simplenote(self.user, self.password).get_note(res[0]["key"])
-        self.assertEqual(0, status)
-        note["content"] = self.unicode_note
-        note, status = Simplenote(self.user, self.password).update_note(note)
-        self.assertEqual(0, status)
-        note, status = Simplenote(self.user, self.password).get_note(note["key"])
-        self.assertEqual(0, status)
-        self.assertTrue(self.is_utf8(note["content"]))
+        if self.unicode_note_key != False:
+            note, status = Simplenote(self.user,
+                                    self.password).get_note(self.unicode_note_key)
+            if status == 0:
+                self.assertTrue(self.is_utf8(note["content"]))
 
     def test_note_with_plus_signs(self):
         note, status = Simplenote(self.user, self.password).add_note("++")
-        self.assertEqual(0, status)
-        note, status = Simplenote(self.user, self.password).get_note(note["key"])
-        self.assertEqual(0, status)
-        self.assertEqual("++", note["content"])
+        if status == 0:
+            note, status = Simplenote(self.user, self.password).get_note(note["key"])
+            if status == 0:
+                self.assertEqual("++", note["content"])
 
     def is_utf8(self, s):
         try:
@@ -139,6 +148,12 @@ class TestSimplenote(unittest.TestCase):
             return True
         except UnicodeDecodeError:
             return False
+
+    def clear_all_notes(self):
+        res, status = Simplenote(self.user, self.password).get_note_list()
+        if status == 0:
+            [Simplenote(self.user, self.password).delete_note(n["key"]) for n in res]
+
 
 
 if __name__ == '__main__':
